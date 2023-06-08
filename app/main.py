db56f845d4ab17724e84e0c05159ac85f9c3b4db
RosePaku <jiinmam12@naver.com>
2023-06-08 21:15:59 +0900

    0608

--- app/main.py
+++ app/main.py
@@ -10,13 +10,26 @@
 # 추가부분2 0607
 # 1) 출력값 result 형태로 변환
 
+# 추가부분3 0608
+# 1) 수정 후 확인해보기 
+# 2) http://localhost:8000/item-based/2
+
+# 추가부분4
+# user-based 엔드포인트에서 평점 정보를 받기
+
 from typing import List, Optional
+from unittest import result
 from fastapi import FastAPI, Query
 
 # 추가1, 추가 2
 from resolver import random_items, random_genres_items
-# 추가2
-from fastapi.middleware.cors import CORSMiddleware
+# # 추가2
+# from fastapi.middleware.cors import CORSMiddleware
+
+# 추가3
+from recommender import item_based_recommendation, user_based_recommendation
+from resolver import random_items, random_genres_items
+
 app = FastAPI()
 
 @app.get("/")
@@ -32,7 +45,7 @@ async def root():
 @app.get("/all/")
 async def all_movies():
     result = random_items()
-    return {"message": result}
+    return {"result": result}
 
 # 이전
 # @app.get("/genres/{genre}")
@@ -46,13 +59,35 @@ async def genre_movies(genre: str):
     return {"result": result}
 
 
+# 이전
+# @app.get("/user-based/")
+# async def user_based(params: Optional[List[str]]=Query(None)):
+#     return {"message": "user based"}
+
+
+# 변경4
 @app.get("/user-based/")
 async def user_based(params: Optional[List[str]]=Query(None)):
-    return {"message": "user based"}
+    input_ratings_dict = dict(
+        (int(x.split(":")[0]), float(x.split(":")[1])) for x in params
+    )
+    result = user_based_recommendation(input_ratings_dict)
+    return {"result": result}
 
 
+
+
+# 이전
+# @app.get("/item-based/{item_id}")
+# async def item_based(item_id: str):
+#     return {"message": f"item based : {item_id}"}
+
+
+# 변경3
 @app.get("/item-based/{item_id}")
 async def item_based(item_id: str):
-    return {"message": f"item based : {item_id}"}
+    result = item_based_recommendation(item_id)
+    return {"result": result}
+
 
 
--- app/recommender.py
+++ app/recommender.py
@@ -1,4 +1,8 @@
-# 실행 문구 mkdir model && python recommender.py 
+# 실행 문구 mkdir model && python recommender.py
+# 추천 알고리즘 
+
+# 추가부분3 0608
+# 1) user_based 부분 추가
 
 import pandas as pd
 import numpy as np
@@ -43,6 +47,38 @@ def model_train():
     
     pickle.dump(als_model, open(saved_model_fname, "wb"))
     return als_model
+
+# 추가 부분3 0608 함수 3개
+def calculate_user_based(user_items,items):
+    loaded_model = pickle.load(open(saved_model_fname,"rb"))
+    recs = loaded_model.recommend(
+        userid=0, user_items=items, recalculate_user=True, N=10
+    )
+    return [str(items[r]) for r in recs[0]]
+
+def build_matrix_input(input_rating_dict,items):
+    model = pickle.load(open(saved_model_fname,"rb"))
+    # input rating list : {1: 4.0, 2: 3.5, 3: 5.0}
+
+    item_ids = {r: i for i, r in items.items()}
+    mapped_idx = [item_ids[s] for s in input_rating_dict.keys() if s in item_ids]
+    data = [weight * float(x) for x in input_rating_dict.values()]
+    rows = [0 for _ in mapped_idx]
+    shape = (1, model.item_factors.shape[0])
+    return coo_matrix((data, (rows, mapped_idx)), shape=shape).tocsr()
+
+def user_based_recommendation(input_ratings):
+    ratings_df = pd.read_csv(data_fname)
+    ratings_df["userId"] = ratings_df["userId"].astype("category")
+    ratings_df["movieId"] = ratings_df["moveId"].astype("category")
+    movies_df = pd.read_csv(item_fname)
+
+    items = dict(enumerate(ratings_df["movieId"].cat.categories))
+    input_matrix = build_matrix_input(input_ratings,items)
+    result = calculate_user_based(input_matrix,items)
+    result = [int(x) for x in result]
+    result_items = movies_df[movies_df["movieId"].isin(result)].to_dict("records")
+    return result_items
 
 # 추가부분2
 # 가장 비슷한 11개의 영화를 결과로 반환하는 함수
